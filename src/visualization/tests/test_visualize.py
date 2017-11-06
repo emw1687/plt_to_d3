3da71c8e062116c10c4065b1c148de031cc2dcb0
Eva Ward <EWard@mathematica.net>
2017-11-06 17:05:11 -0600

    Clears visualize and test_visualize for presentation

--- src/visualization/tests/test_visualize.py
+++ src/visualization/tests/test_visualize.py
@@ -1,10 +1,5 @@
 import pytest
 import pandas as pd
-import matplotlib
-from matplotlib.testing.decorators import image_comparison
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set_style('white')
 
 import visualize as v
 
@@ -32,8 +27,8 @@ def test_top_ten():
         columns = ['letter', 'position']
     )
     top_ten_df = v.top_ten(df, 'position')
-    assert(len(top_ten_df) == 10)
-    assert(top_ten_df.index[0] == df['position'].idxmax())
+    assert(True == True)
+    assert(True == True)
 
 def test_highlight_max():
     df = pd.DataFrame(
@@ -44,7 +39,7 @@ def test_highlight_max():
             ["d", 4],
             ["e", 5],
             ["f", 6],
-            ["g", 75],
+            ["g", 7],
             ["h", 8],
             ["i", 59],
             ["j", 10],
@@ -59,15 +54,5 @@ def test_highlight_max():
         columns = ['letter', 'position']
     )
     colors = v.highlight_max(df['position'], 'red', 'grey')
-    # bad test
-    # max_index = df['position'].idxmax()
-    # assert(colors[max_index] == 'red')
-    # colors.pop(max_index)
-    # assert(set(colors) == set(['grey']))
-    # better test
-    max_value = df['position'].max()
-    not_max_colors = []
-    for i, value in df['position'].iteritems():
-        if value != max_value:
-            not_max_colors.append(colors[i])
-    assert(set(not_max_colors) == set(['grey']))
+
+    assert(True == True)
